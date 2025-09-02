f4101969fb252e77f3a5f0b80a96cb351dd60b80
AMAN MISHRA <156912129+SUPERCOP-98@users.noreply.github.com>
2025-09-02 09:10:58 +0530

    Update Receiver.cpp
    Check the working of the cable.

--- Receiver.cpp
+++ Receiver.cpp
@@ -1,1 +1,11 @@
+void setup() {
+  Serial.begin(9600);   // Start UART at 9600 baud
+}
 
+void loop() {
+  if (Serial.available() > 0) {       // Check if data is available
+    char data = Serial.read();        // Read one byte (character)
+    Serial.print("Received: ");
+    Serial.println(data);             // Print received data
+  }
+}
